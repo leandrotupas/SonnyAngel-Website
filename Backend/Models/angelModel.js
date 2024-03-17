fae1eff1344397fc1d35421c9aeb4a35a4f8bc2c
leandrotupas <tupas.leandro.lt@gmail.com>
2024-03-17 20:12:35 +0800

    connected mongodb

--- Backend/Models/angelModel.js
+++ Backend/Models/angelModel.js
@@ -1,1 +1,22 @@
-export const Angel = mongoose.model('Angel', {name: String});
+import mongoose from "mongoose";
+
+const angelSchema = mongoose.Schema( {
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    stock: {
+        type: Number,
+        required: true,
+    },
+    avail: {
+        type: Boolean,
+        required: true,
+    }
+});
+
+export const Angel = mongoose.model('Angel', angelSchema);
